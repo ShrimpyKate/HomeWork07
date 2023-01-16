a3123928b4046dcc217e79eb52184a6b6743f461
Kate <katekrupnik@gmail.com>
2023-01-17 00:46:09 +0300

    cod for ex1

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+
+//1.задать длинну массива
+//2.создать массив
+//2.наполнить массив
+//3.напечатать массив
+//
+
+(int, int) GetSizeByUser()
+{
+  Console.WriteLine("Введите размер массива: ");
+  int row = int.Parse(Console.ReadLine());
+  int columns = int.Parse(Console.ReadLine());
+return (row, columns);
+}
+
+double[,] CreateMatrix(int row, int columns)
+{
+  double[,] matr = new double[row, columns];
+  return matr;
+}
+
+void FillandPrintMatrix(double [,]matr)
+{
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      matr[i,j] = Random.Shared.Next(0, 10);
+       Console.Write(matr[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+
+(int row, int columns) size = GetSizeByUser();
+double[,] matrix = CreateMatrix(size.row, size.columns);
+FillandPrintMatrix(matrix);
+
