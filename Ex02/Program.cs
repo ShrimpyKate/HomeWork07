bd686e54dfcf89f11c4fa7fdc534c1095e3fe071
Kate <katekrupnik@gmail.com>
2023-01-18 22:44:24 +0300

    finish cod ex2

--- Ex02/Program.cs
+++ Ex02/Program.cs
@@ -2,27 +2,33 @@
 //позиции элемента в двумерном массиве, и возвращает
 // значение этого элемента или же указание, что такого элемента нет.
 
+(int, int) GetSizeByUser()
+{
+  int row = Convert.ToInt32(Console.ReadLine());
+  int columns = Convert.ToInt32(Console.ReadLine());
+return (row, columns);
+}
+
 int[,] GenerateMatrix(int rows, int columns)
 {
   int[,] arr = new int[rows, columns];
-
 
   for (int r = 0; r < rows; r++)
   {
     for (int c = 0; c < columns; c++)
     {
-      arr[r, c] = Random.Shared.Next(0, 10);
+      arr[r, c] = Convert.ToInt32(new Random().Next(0,100));
     }
   }
-
   return arr;
 }
 
 int FindElement(int[,] matr, int row, int column)
 {
-  if (row >= matr.Length && row < 0 && column >= matr.GetLength(1) || column < 0)
+  if (row >= matr.GetLength(0) || row < 0 || column >= matr.GetLength(1) || column < 0)
   {
     Console.WriteLine("Element not found!");
+    return -1;
   }
   return matr[row, column];
 }
@@ -39,7 +45,10 @@ void Print2DArray(int[,] matr)
     Console.WriteLine();
   }
 }
-
-int[,] matrix = GenerateMatrix(5, 5);
+Console.WriteLine("Введите размер массива: ");
+(int row, int columns) size = GetSizeByUser();
+int[,] matrix = GenerateMatrix(size.row, size.columns);
 Print2DArray(matrix);
-Console.WriteLine(FindElement(matrix, 2, 2));
+Console.WriteLine("Введите индексы искомого элемента: ");
+(int row, int columns) findEl = GetSizeByUser();
+Console.WriteLine(FindElement(matrix, findEl.row, findEl.columns));
